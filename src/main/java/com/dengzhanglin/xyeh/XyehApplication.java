ca8dbb11437511f5fe739e059a396e77a27bf997
zhanglindeng <zhanglindeng@outlook.com>
2018-05-03 11:46:39 +0800

    init code

---
+++ src/main/java/com/dengzhanglin/xyeh/XyehApplication.java
@@ -0,0 +1,12 @@
+package com.dengzhanglin.xyeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyehApplication.class, args);
+    }
+}
